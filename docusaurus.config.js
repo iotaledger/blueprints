9dbca56fc40aad652073cab47d41f3843a344dac
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-12 09:22:33 +0000

    Prepare for new wiki build system

---
+++ docusaurus.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+ module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'blueprints',
+                path: path.resolve(__dirname, './'),
+                routeBasePath: 'blueprints',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/blueprints/edit/main',
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
